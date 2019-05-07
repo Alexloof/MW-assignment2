7a8f03b94fe10f3a3a709bd31230bac91d2c742a
Alex Lööf <alex.loofet@gmail.com>
2019-05-07 23:09:46 +0200

    added migrations

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/products', 'ProductsController@index');
+$router->get('/products/{id}', 'ProductsController@show');
